b771d5401f7c85a136b3ad6f9b37ce14f95352cd
Alejandra Camelo Cruz <camelo.cruz@leibniz-zas.de>
2024-09-25 17:19:39 +0200

    working

--- train.py
+++ train.py
@@ -27,6 +27,13 @@ num_epochs = 100
 num_classes = 3
 embedding_dim = 128
 
+vocab_size = preprocessing.get_vocab_size()
+
+train_losses = []
+test_losses = []
+train_accuracies = []
+test_accuracies = []
+
 train_sample, test_sample = preprocessing.preprocess_data(csv_path, input_len)
 train_dataset = TextDataset(train_sample['input_ids'], train_sample['attention_mask'], train_sample['labels'])
 test_dataset = TextDataset(test_sample['input_ids'], test_sample['attention_mask'], test_sample['labels'])
@@ -46,52 +53,52 @@ optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
 for epoch in range(num_epochs):
     model.train()
-        running_train_loss = 0.0
-        correct_train_predictions = 0
+    running_train_loss = 0.0
+    correct_train_predictions = 0
 
-        for batch in train_dataloader:
+    for batch in train_dataloader:
+        input_ids = batch['input_ids'].to(device)
+        labels = batch['labels'].to(device)
+
+        optimizer.zero_grad()
+        outputs = model(input_ids)            
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_train_loss += loss.item()
+        _, preds = torch.max(outputs, dim=1)
+        correct_train_predictions += torch.sum(preds == labels)
+
+    accuracy_train = correct_train_predictions.double() / len(train_dataset)
+    loss_train = running_train_loss / len(train_dataloader)
+
+    train_losses.append(loss_train)
+    train_accuracies.append(accuracy_train.item())
+
+    model.eval()
+    running_test_loss = 0.0
+    correct_test_predictions = 0
+
+    with torch.no_grad():
+        for batch in test_dataloader:
             input_ids = batch['input_ids'].to(device)
             labels = batch['labels'].to(device)
 
-            optimizer.zero_grad()
             outputs = model(input_ids)
+
             loss = criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
+            running_test_loss += loss.item()
 
-            running_train_loss += loss.item()
             _, preds = torch.max(outputs, dim=1)
-            correct_train_predictions += torch.sum(preds == labels)
+            correct_test_predictions += torch.sum(preds == labels)
 
-        accuracy_train = correct_train_predictions.double() / len(train_dataset)
-        loss_train = running_train_loss / len(train_dataloader)
+    epoch_test_loss = running_test_loss / len(test_dataloader)
+    epoch_test_acc = correct_test_predictions.double() / len(test_dataset)
 
-        train_losses.append(loss_train)
-        train_accuracies.append(accuracy_train.item())
+    test_losses.append(epoch_test_loss)
+    test_accuracies.append(epoch_test_acc.item())
 
-        model.eval()
-        running_test_loss = 0.0
-        correct_test_predictions = 0
-
-        with torch.no_grad():
-            for batch in test_dataloader:
-                input_ids = batch['input_ids'].to(device)
-                labels = batch['labels'].to(device)
-
-                outputs = model(input_ids)
-
-                loss = criterion(outputs, labels)
-                running_test_loss += loss.item()
-
-                _, preds = torch.max(outputs, dim=1)
-                correct_test_predictions += torch.sum(preds == labels)
-
-        epoch_test_loss = running_test_loss / len(test_dataloader)
-        epoch_test_acc = correct_test_predictions.double() / len(test_dataset)
-
-        test_losses.append(epoch_test_loss)
-        test_accuracies.append(epoch_test_acc.item())
-
-        print(f'Epoch {epoch+1}/{num_epochs}, '
-            f'Train Loss: {loss_train:.4f}, Train Accuracy: {accuracy_train:.4f}, '
-            f'Test Loss: {epoch_test_loss:.4f}, Test Accuracy: {epoch_test_acc:.4f}')
+    print(f'Epoch {epoch+1}/{num_epochs}, '
+        f'Train Loss: {loss_train:.4f}, Train Accuracy: {accuracy_train:.4f}, '
+        f'Test Loss: {epoch_test_loss:.4f}, Test Accuracy: {epoch_test_acc:.4f}')
